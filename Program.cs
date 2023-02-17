07ede3465808cf8bfffeb2c3fa15a3641df9ce65
PIENNU <114809507+piennu777@users.noreply.github.com>
2023-02-17 23:21:57 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using EasyTabs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new main());
+            AppContainer container = new AppContainer();
+            container.Tabs.Add(new EasyTabs.TitleBarTab(container)
+            {
+                Content = new Form1
+                {
+                    Text = "新しいタブ"
+                }
+            });
+            container.SelectedTabIndex = 0;
+            TitleBarTabsApplicationContext applicationContext = new TitleBarTabsApplicationContext();
+            applicationContext.Start(container);
+            Application.Run(applicationContext);
+        }
+    }
+}
